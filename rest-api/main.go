1f7cf581301ba45ca158ff9ce04ad7b96eb6c878
nickkhall <nick.hall@bluelogicteam.com>
2019-02-11 22:01:04 -0600

    fixed conflicts

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
+
+	"google.golang.org/api/option"
 )
 
 func main() {
+	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+
+	client, err := app.Firestore(context.Background())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	router := mux.NewRouter()
-	router.HandleFunc("/todos", GetAllTodos).Methods("GET")
+	router.HandleFunc("/todos/{id}", GetTodo).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":3000", router))
+
+	defer client.Close()
 }
--- rest-api/todo.go
+++ rest-api/todo.go
@@ -1,31 +1,25 @@
 package main
 
 import (
-	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Todo Struct : Data structure for a Todo
 type Todo struct {
-	ID        string `json:"id",omitempty`
-	Name      string `json:"name,omitempty`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
 	Completed bool   `json:"completed"`
 }
 
-var todos []Todo
+// GetTodo : Gets a single todo
+func GetTodo(w http.ResponseWriter, r *http.Request) {
+	todoID := mux.Vars(r)["id"]
 
-// GetAllTodos : Returns all current Todos
-func GetAllTodos(w http.ResponseWriter, r *http.Request) {
-	todos = append(todos, Todo{
-		ID:        "1234",
-		Name:      "take out the trash",
-		Completed: false,
-	})
-	todos = append(todos, Todo{
-		ID:        "1235",
-		Name:      "sweep the cat food",
-		Completed: true,
-	})
-
-	json.NewEncoder(w).Encode(todos)
+	dsnap, err := client.Collection("react-redux-todos").Doc("todos").Get(todoId)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
