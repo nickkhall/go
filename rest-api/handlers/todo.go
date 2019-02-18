f17882973407a3c4cd271387d4d8308dbdae7416
nickhallph <nick.hall@yum.com>
2019-02-17 23:44:53 -0600

    wip

---
+++ rest-api/handlers/todo.go
@@ -0,0 +1,47 @@
+package todo
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Todo Struct : Data structure for a Todo
+type Todo struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+}
+
+// PopulateTodos : Populates todo slice
+func PopulateTodos() []Todo {
+	todos := []Todo{
+		{
+			ID:        "1234",
+			Name:      "Clean house",
+			Completed: false,
+		},
+		{
+			ID:        "1233",
+			Name:      "learn golang",
+			Completed: false,
+		},
+	}
+
+	return todos
+}
+
+// GetTodo : Gets a single todo
+func GetTodo(w http.ResponseWriter, r *http.Request) {
+	todoID := mux.Vars(r)["id"]
+	todos := PopulateTodos()
+
+	for _, todo := range todos {
+		fmt.Println(todo)
+		if todo.ID == todoID {
+			json.NewEncoder(w).Encode(todo)
+		}
+	}
+}
--- rest-api/main.go
+++ rest-api/main.go
@@ -1,30 +1,16 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
-	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
-
-	"google.golang.org/api/option"
+	todo "github.com/nickkhall/go/rest-api/handlers"
 )
 
 func main() {
-	opt := option.WithCredentialsFile("serviceAccountKey.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-
-	client, err := app.Firestore(context.Background())
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	router := mux.NewRouter()
-	router.HandleFunc("/todos/{id}", GetTodo).Methods("GET")
+	router.HandleFunc("/todos/{id}", todo.GetTodo).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
-
-	defer client.Close()
 }
